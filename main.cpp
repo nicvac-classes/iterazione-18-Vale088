bbf6b567f663999b29c062e59e5ffdb71c7693b0
Vale088 <valerio.sinni@marconibari.edu.it>
2024-11-06 19:46:48 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,26 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+   int i;
+   float m,somma,media;
+   somma=0;
+   for(i=1;i<=7;i++);  
+   {
+    cout<<"inserire il livello dell'acqua "<<endl;
+    cin>>m;
+    if(m>10); {
+        cout<<"nel giorno"&i&"il livello dell'acqua è alto"<<endl;
+
+    } 
+    somma=(m)+somma;
+    media=somma/7;
+    if(media>0) {
+        cout<<"acqua alta"<<endl;
+    }
+    if(media<=0){
+        cout<<"livello normale"<<endl;
+    }
+   }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
